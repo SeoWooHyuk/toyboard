a836902d9d8d893044090e11934969da830387b6
서우혁 <tjdngur123@naver.com>
2023-07-15 01:10:08 +0900

    json api 데이터 자바객체로파싱공부중

--- src/main/java/com/restfulproject/toyboard/ToyboardApplication.java
+++ src/main/java/com/restfulproject/toyboard/ToyboardApplication.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
 
@@ -15,6 +17,12 @@ public class ToyboardApplication {
 		
 		SpringApplication.run(ToyboardApplication.class, args);
 
+	}
+
+	@Bean
+	MappingJackson2JsonView jsonView()
+	{
+		return new MappingJackson2JsonView();
 	}
 
 }
--- src/main/java/com/restfulproject/toyboard/openapi/controller/CallcampinApiController.java
+++ src/main/java/com/restfulproject/toyboard/openapi/controller/CallcampinApiController.java
@@ -1,27 +1,30 @@
 package com.restfulproject.toyboard.openapi.controller;
 
-import org.json.JSONObject;
-import org.json.XML;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.restfulproject.toyboard.openapi.domain.Camping;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
-public class CallcampinApiController {
+public class CallcampinApiController  {
     
    @GetMapping("/jsonapi")
     public String callApiWithJson() {
         StringBuffer result = new StringBuffer();
         String jsonPrintString = null;
-
+       
         try {
             String apiUrl = "https://api.odcloud.kr/api/15111395/v1/uddi:8c528230-eda4-4d83-855a-bee73605e49f?"+
             "page=1"+
@@ -34,22 +37,27 @@ public class CallcampinApiController {
             BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, "UTF-8"));
 
+
             String returnLine;
             while((returnLine = bufferedReader.readLine()) != null) {
                 result.append(returnLine);
             }
-
+            
             jsonPrintString = result.toString();
 
-            ObjectMapper objectMapper = new ObjectMapper();
 
-            JsonNode jsonNode = objectMapper.readTree(jsonPrintString);
+            //밑에하고 똑같은 역활
+            // Gson gson = new Gson(); 
+            // JsonElement root = gson.fromJson(jsonPrintString, JsonElement.class);
+            // JsonObject jsonObj = root.getAsJsonObject();
+            // System.out.println(jsonObj);
+    
+            // ObjectMapper mapper = new ObjectMapper();
+            // Camping data = mapper.readValue( jsonPrintString , Camping.class );
 
-            JsonNode arrayNode = jsonNode.isArray() ? jsonNode : jsonNode.get("array");
+            List<Camping> personList = new ArrayList<>();
+     
 
-        
-            
-        
         } catch (Exception e) {
             e.printStackTrace();
         }
